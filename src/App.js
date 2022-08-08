326c77e98d0433bd0cc34707047e9d18f2e4696e
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2022-08-08 16:16:15 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import GlobalStyle from "./styles/global";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { Teste } from "./pages/Teste";
-import { Instrucoes } from "./pages/Intrucoes";
+import { Instrucoes } from "./pages/Instrucoes";
+
+import "./styles/Global.css";
+
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<Instrucoes />}></Route>
-        <Route path="/teste" element={<Teste />}></Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Instrucoes />}></Route>
+          <Route path="/teste" element={<Teste />}></Route>
+        </Routes>
     </BrowserRouter>
   );
 };
